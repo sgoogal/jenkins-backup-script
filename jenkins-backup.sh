295dfc297f3bcc1e6ad544a80b43da1ff6bd2ffe
sgoogal <sangameshg88@gmail.com>
2018-02-15 17:22:00 +0530

    Update jenkins-backup.sh

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -53,9 +53,12 @@ function backup_jobs {
     find . -maxdepth 1 -type d | while read job_name ; do
       [ "$job_name" = "." ] && continue
       [ "$job_name" = ".." ] && continue
-      [ -d "$JENKINS_HOME/jobs/$rel_depth/$job_name" ] && mkdir -p "$ARC_DIR/jobs/$rel_depth/$job_name/"
-    #  find "$JENKINS_HOME/jobs/$rel_depth/$job_name/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} cp {} "$ARC_DIR/jobs/$rel_depth/$job_name/"
-      find "$JENKINS_HOME/jobs/$rel_depth/$job_name/" -maxdepth 1 \( -name "*.xml" -o -name "lastSuccessful" \) -print0 | xargs -0 -I {} cp -r {} "$ARC_DIR/jobs/$rel_depth/$job_name/"
+      [ -d "$JENKINS_HOME/jobs/$rel_depth/$job_name" ] && mkdir -p "$ARC_DIR/jobs/$rel_depth/$job_name/builds"
+      build=ls -ltr "$JENKINS_HOME/jobs/$rel_depth/$job_name/builds" | tail -1 | awk '{print $NF}'
+      find "$JENKINS_HOME/jobs/$rel_depth/$job_name/" -maxdepth 1 -name "*.xml" -print0 | xargs -0 -I {} cp {} "$ARC_DIR/jobs/$rel_depth/$job_name/"
+      if [ -n "$build" ] ; then
+      cp -r "$build" "$ARC_DIR/jobs/$rel_depth/$job_name/builds/"
+      fi
       if [ -f "$JENKINS_HOME/jobs/$rel_depth/$job_name/config.xml" ] && [ "$(grep -c "com.cloudbees.hudson.plugins.folder.Folder" "$JENKINS_HOME/jobs/$rel_depth/$job_name/config.xml")" -ge 1 ] ; then
         #echo "Folder! $JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
         backup_jobs "$JENKINS_HOME/jobs/$rel_depth/$job_name/jobs"
